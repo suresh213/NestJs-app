84856f2cb07de1e4727901aa2a003b442427b83f
suresh213 <asuresh52012@gmail.com>
2023-12-27 23:14:36 +0530

    Change port

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function app() {
-  const PORT = process.env.PORT;
+  const PORT = 8000;
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
   console.log(`Server is running at port ${PORT}`);
